db1c1daf29dd2ef4fad1b051219016a2ab266459
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-31 11:07:28 +0100

    Move many options to setup.cfg

--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@ from skbuild import setup
 from setuptools import find_packages
 
 setup(
-    name="quirc",
-    version="0.1.0",
-    description="Python bindings for quirc, via pybind11",
-    author="Maximilian Nöthe",
-    author_email="maximilian.noethe@tu-dortmund.de",
-    license="MIT",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     cmake_install_dir="src/quirc",
-    extras_require={"test": ["pytest"]},
-    python_requires=">=3.7",
+    cmake_args=[
+        '-DCPP_EXAMPLE:BOOL=OFF',
+    ],
 )
